93e32dc9c38451b73d7ac9b59958a155af905bb0
Karl Wright <kwright@apache.org>
2010-04-26 12:28:14 +0000

    First cut of executecommand unix shell script.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lcf/trunk@938018 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/framework/scripts/executecommand.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -e
+
+#Make sure environment variables are properly set
+if [ -e $JAVA_HOME/bin/java ] ; then
+    if [ -f $LCF_HOME/properties.ini ] ; then
+    
+        # Build the classpath
+        CLASSPATH=""
+        for filename in $(ls -1 "$LCF_HOME/processes/jar") ; do
+            if [ -n "$CLASSPATH" ] ; then
+                CLASSPATH="$CLASSPATH":"$LCF_HOME"/processes/jar/"$filename"
+            else
+                CLASSPATH="$LCF_HOME"/processes/jar/"$filename"
+            fi
+        done
+        
+        # Build the defines
+        DEFINES=""
+        for filename in $(ls -1 "$LCF_HOME/processes/define") ; do
+            DEFINEVAR=-D"$filename"=$(cat "$LCF_HOME/processes/define/"$filename")
+            DEFINES="$DEFINES $DEFINEVAR"
+        done
+        
+        "$JAVA_HOME/bin/java" "$DEFINES" --classpath "$CLASSPATH" "$@"
+        exit $?
+        
+    else
+        echo "Environment variable LCF_HOME is not properly set." 1>&2
+        exit 1
+    fi
+    
+else
+    echo "Environment variable JAVA_HOME is not properly set." 1>&2
+    exit 1
+fi
