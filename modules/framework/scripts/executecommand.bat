6fcd5d0e16340c1e9f0aa683be3592445ba95291
Karl Wright <kwright@apache.org>
2010-04-26 11:47:12 +0000

    Update script to check properly for missing variables, and get the quotations right (I hope)...
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lcf/trunk@938013 13f79535-47bb-0310-9956-ffa450edef68

--- modules/framework/scripts/executecommand.bat
+++ modules/framework/scripts/executecommand.bat
@@ -1,12 +1,22 @@
 @echo off
 rem check that JAVA_HOME and LCF_HOME are set
+if not exist "%JAVA_HOME%\bin\java.exe" goto nojavahome
+if not exist "%LCF_HOME%\properties.ini" goto nolcfhome
 rem save existing path here
 set OLDDIR=%CD%
-cd %LCF_HOME%\processes
+cd "%LCF_HOME%\processes"
 set CLASSPATH=.
 for %%f in (jar/*) do call script\setclasspath.bat %%f
 set JAVADEFINES=
 for %%g in (define/*) do call script\setdefine.bat %%g
 rem restore old path here
-cd %OLDDIR%
-"%JAVA_HOME%\bin\java" -Dorg.apache.lcf.configfile=%LCF_HOME%\properties.ini %JAVADEFINES% -classpath %CLASSPATH% %1 %2 %3 %4 %5 %6 %7 %8 %9
+cd "%OLDDIR%"
+"%JAVA_HOME%\bin\java" "-Dorg.apache.lcf.configfile=%LCF_HOME%\properties.ini" %JAVADEFINES% -classpath "%CLASSPATH%" %1 %2 %3 %4 %5 %6 %7 %8 %9
+goto done
+:nojavahome
+echo Environment variable JAVA_HOME is not set properly.
+goto done
+:nolcfhome
+echo Environment variable LCF_HOME is not set properly.
+goto done
+:done
