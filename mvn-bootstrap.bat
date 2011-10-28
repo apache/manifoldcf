74b6c7467b636833a0cc2d8ad5ae0ba931b4e32d
Karl Wright <kwright@apache.org>
2011-10-28 12:35:36 +0000

    Missed changes related to CONNECTORS-283.  The mvn-bootstrap scripts need to be updated when versions of what they install are changed.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lcf/trunk@1190276 13f79535-47bb-0310-9956-ffa450edef68

--- mvn-bootstrap.bat
+++ mvn-bootstrap.bat
@@ -21,7 +21,7 @@ mvn install:install-file -Dfile=lib/hsqldb.jar -DgroupId=org.hsqldb -DartifactId
 
 mvn install:install-file -Dfile=lib/commons-httpclient-mcf.jar -DgroupId=commons-httpclient -DartifactId=commons-httpclient-mcf -Dversion=3.1  -Dpackaging=jar
 mvn install:install-file -Dfile=lib/xercesImpl-mcf.jar -DgroupId=xerces -DartifactId=xercesImpl-mcf -Dversion=2.9.1  -Dpackaging=jar
-mvn install:install-file -Dfile=connectors/jcifs/jcifs/jcifs.jar -DgroupId=org.samba.jcifs -DartifactId=jcifs -Dversion=1.3.16  -Dpackaging=jar
+mvn install:install-file -Dfile=connectors/jcifs/jcifs/jcifs.jar -DgroupId=org.samba.jcifs -DartifactId=jcifs -Dversion=1.3.17  -Dpackaging=jar
 
 cmd /c mvn install:install-file -Dfile=lib/chemistry-opencmis-server-inmemory-war.war -DgroupId=org.apache.chemistry.opencmis -DartifactId=chemistry-opencmis-server-inmemory-war -Dversion=0.5.0 -Dpackaging=war
 
