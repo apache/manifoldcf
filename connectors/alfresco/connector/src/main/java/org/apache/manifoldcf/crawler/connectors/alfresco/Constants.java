7687fd87da5f17363b56927c374249e3b3f6c14e
Piergiorgio Lucidi <piergiorgio@apache.org>
2011-12-16 11:49:28 +0000

    (CONNECTORS-325): Adeguate the content identifier with the Alfresco node reference
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lcf/trunk@1215099 13f79535-47bb-0310-9956-ffa450edef68

--- connectors/alfresco/connector/src/main/java/org/apache/manifoldcf/crawler/connectors/alfresco/Constants.java
+++ connectors/alfresco/connector/src/main/java/org/apache/manifoldcf/crawler/connectors/alfresco/Constants.java
@@ -1,3 +1,6 @@
+/* $Id$ */
+
+
 /**
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements. See the NOTICE file distributed with
@@ -16,8 +19,12 @@
  */
 package org.apache.manifoldcf.crawler.connectors.alfresco;
 
+
 public class Constants extends org.alfresco.webservice.util.Constants {
   
   public static final String PROP_VERSION_LABEL = createQNameString(NAMESPACE_CONTENT_MODEL, "versionLabel");
-  
+  public static final String PROP_NODE_UUID = createQNameString(NAMESPACE_SYSTEM_MODEL, "node-uuid");
+  public static final String PROP_STORE_PROTOCOL = createQNameString(NAMESPACE_SYSTEM_MODEL, "store-protocol");
+  public static final String PROP_STORE_ID = createQNameString(NAMESPACE_SYSTEM_MODEL, "store-identifier");
+
 }
