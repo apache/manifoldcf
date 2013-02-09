f06424aba5b221801431945d1e4de36e52d72dfb
Karl Wright <kwright@apache.org>
2013-02-09 14:57:23 +0000

    Add base XML parsing classes equivalent to what we've been using with the SAX parser.
    git-svn-id: https://svn.apache.org/repos/asf/manifoldcf/branches/CONNECTORS-633@1444375 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/core/src/main/java/org/apache/manifoldcf/core/fuzzyml/XMLFuzzyHierarchicalParseState.java
@@ -0,0 +1,141 @@
+/* $Id$ */
+
+/**
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements. See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* The ASF licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License. You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package org.apache.manifoldcf.core.fuzzyml;
+
+import org.apache.manifoldcf.core.interfaces.*;
+import java.io.*;
+import java.util.*;
+
+/** Class to keep track of XML hierarchy in the face of possibly corrupt
+* XML and with case-insensitive tags, etc.
+* Basically, this class accepts what is supposedly XML but allows for various
+* kinds of handwritten corruption.  Specific kinds of errors allowed include:
+*
+* - Bad character encoding
+* - Tag case match problems; all attributes are (optionally) bashed to lower case,
+*    and tag names are checked to match when all lower case, if case-sensitive didn't
+*    work
+* - End tag matching problems, where someone lost an end tag somehow
+* - Other parsing recoveries to be added as they arise
+*
+* The functionality of this class is also somewhat lessened vs. standard
+* SAX-type parsers.  No namespace interpretation is done, for instance; tag qnames
+* are split into namespace name and local name, and that's all folks.  But if you need
+* more power, you can write a class extension that will do that readily.
+*/
+public class XMLFuzzyHierarchicalParseState extends XMLFuzzyParseState
+{
+  /** The current context */
+  protected XMLParsingContext currentContext = null;
+
+  /** Constructor.
+  */
+  public XMLFuzzyHierarchicalParseState(boolean lowerCaseAttributes, boolean lowerCaseTags,
+    boolean lowerCaseQAttributes, boolean lowerCaseQTags,
+    boolean lowerCaseBTags, boolean lowerCaseEscapeTags)
+  {
+    super(lowerCaseAttributes,lowerCaseTags,lowerCaseQAttributes,lowerCaseQTags,lowerCaseBTags,lowerCaseEscapeTags);
+  }
+  
+  public void setContext(XMLParsingContext context)
+  {
+    currentContext = context;
+  }
+
+  public XMLParsingContext getContext()
+  {
+    return currentContext;
+  }
+
+  /** Call this method to clean up completely after a parse attempt, whether successful or failure. */
+  public void cleanup()
+    throws ManifoldCFException
+  {
+    // This sets currentContext == null as a side effect, unless an error occurs during cleanup!!
+    currentContext.cleanup();
+  }
+
+  /** Map version of the noteTag method.
+  *@return true to halt further processing.
+  */
+  @Override
+  protected boolean noteTagEx(String tagName, String nameSpace, String localName, Map<String,String> attributes)
+    throws ManifoldCFException
+  {
+    // MHL
+    return false;
+  }
+
+  /** Note end tag.
+  */
+  @Override
+  protected boolean noteEndTagEx(String tagName, String nameSpace, String localName)
+    throws ManifoldCFException
+  {
+    // MHL
+    return false;
+  }
+
+  /** This method gets called for every character that is not part of a tag etc.
+  * Override this method to intercept such characters.
+  *@return true to halt further processing.
+  */
+  @Override
+  protected boolean noteNormalCharacter(char thisChar)
+    throws ManifoldCFException
+  {
+    // MHL
+    return false;
+  }
+
+  /** New version of the noteEscapedTag method.
+  *@return true to halt further processing.
+  */
+  protected boolean noteEscapedEx(String token)
+    throws ManifoldCFException
+  {
+    // MHL
+    return false;
+  }
+  
+  /** This method gets called for every character that is found within an
+  * escape block, e.g. CDATA.
+  * Override this method to intercept such characters.
+  *@return true to halt further processing.
+  */
+  @Override
+  protected boolean noteEscapedCharacter(char thisChar)
+    throws ManifoldCFException
+  {
+    // MHL
+    return false;
+  }
+
+  /** Called for the end of every cdata-like tag.
+  *@return true to halt further processing.
+  */
+  @Override
+  protected boolean noteEndEscaped()
+    throws ManifoldCFException
+  {
+    // MHL
+    return false;
+  }
+  
+}
