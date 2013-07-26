2fcf46e808905a78818d8ad8ae3ac0f4253ddde0
Karl Wright <kwright@apache.org>
2013-07-26 19:23:44 +0000

    Add security page.
    git-svn-id: https://svn.apache.org/repos/asf/manifoldcf/site/trunk@1507408 13f79535-47bb-0310-9956-ffa450edef68

--- scripts/sitepatch.bat
+++ scripts/sitepatch.bat
@@ -3,3 +3,5 @@
 "%JAVA_HOME%"\bin\java -jar c:\javadocpatcher\JavadocUpdaterTool.jar -R %1\release\release-1.1.1\api
 "%JAVA_HOME%"\bin\java -jar c:\javadocpatcher\JavadocUpdaterTool.jar -R %1\release\release-1.0.1\api
 "%JAVA_HOME%"\bin\java -jar c:\javadocpatcher\JavadocUpdaterTool.jar -R %1\release\release-0.6\api
+del /s %1\*.orig
+
