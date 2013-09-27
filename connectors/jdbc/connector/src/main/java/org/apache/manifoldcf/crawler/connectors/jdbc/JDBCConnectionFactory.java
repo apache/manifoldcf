082a95a9fe73df204598d844ce8a8d58677e249d
Karl Wright <kwright@apache.org>
2013-09-27 00:21:22 +0000

    Fix compilation error
    git-svn-id: https://svn.apache.org/repos/asf/manifoldcf/branches/CONNECTORS-13@1526758 13f79535-47bb-0310-9956-ffa450edef68

--- connectors/jdbc/connector/src/main/java/org/apache/manifoldcf/crawler/connectors/jdbc/JDBCConnectionFactory.java
+++ connectors/jdbc/connector/src/main/java/org/apache/manifoldcf/crawler/connectors/jdbc/JDBCConnectionFactory.java
@@ -50,7 +50,7 @@ public class JDBCConnectionFactory
     driverMap.put("mysql:","com.mysql.jdbc.Driver");
     try
     {
-      _pool = new ConnectionPoolManager(120);
+      _pool = new ConnectionPoolManager(120,false);
     }
     catch (Exception e)
     {
