3804eebf7b6f5f629dc2431cfa41a47b9ad9b02d
Karl Wright <kwright@apache.org>
2013-10-22 19:33:34 +0000

    Import email connector from github, minus the pom and build.xml
    git-svn-id: https://svn.apache.org/repos/asf/manifoldcf/branches/CONNECTORS-553@1534748 13f79535-47bb-0310-9956-ffa450edef68

---
+++ connectors/email/connector/src/main/java/org/apache/manifoldcf/crawler/connectors/email/EmailConfig.java
@@ -0,0 +1,94 @@
+/**
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements. See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* The ASF licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License. You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package org.apache.manifoldcf.crawler.connectors.email;
+
+
+/**
+* Parameters data for the Email repository connector.
+*/
+public class EmailConfig {
+
+  /**
+  * Username
+  */
+  public static final String USERNAME_PARAM = "username";
+
+  /**
+  * Password
+  */
+  public static final String PASSWORD_PARAM = "password";
+
+  /**
+  * Protocol
+  */
+  public static final String PROTOCOL_PARAM = "protocol";
+
+  /**
+  * Server name
+  */
+  public static final String SERVER_PARAM = "server";
+
+  /**
+  * Port
+  */
+  public static final String PORT_PARAM = "port";
+
+  /**
+  * Properties
+  */
+  public static final String PROPERTIES_PARAM = "properties";
+
+  public static final String PROTOCOL_DEFAULT_VALUE = "IMAP";
+  public static final String PORT_DEFAULT_VALUE = "25";
+
+
+  public static final String NODE_PROPERTIES = "properties";
+  public static final String NODE_METADATA = "metadata";
+  public static final String NODE_FILTER = "filter";
+  public static final String SERVER_PROPERTY = "serverproperty";
+  public static final String VALUE = "p_value";
+  public static final String ATTRIBUTE_NAME = "name";
+
+  public static final String ATTRIBUTE_VALUE = "value";
+  public static final String[] BASIC_METADATA = {"To","From","Subject","Body","Date","Encoding of Attachment","MIME type of attachment"};
+  public static final String[] BASIC_SEARCHABLE_ATTRIBUTES = {"To","From","Subject","Body","Date","Folder"};
+  protected final static String ACTIVITY_FETCH = "fetch";
+
+
+  public static final String RELATIONSHIP_CHILD = "child";
+  public static final String PROTOCOL_IMAP = "imap";
+  public static final String PROTOCOL_IMAP_PROVIDER = "imaps";
+  public static final String PROTOCOL_POP3 = "POP3";
+  public static final String PROTOCOL_POP3_PROVIDER = "pop3";
+  public static final String FOLDER_INBOX = "INBOX";
+  public static final String EMAIL_SUBJECT = "subject";
+  public static final String EMAIL_FROM = "from";
+  public static final String EMAIL_TO = "to";
+  public static final String EMAIL_BODY = "body";
+  public static final String ATTRIBUTE_FOLDER = "Folder";
+  public static final String EMAIL_DATE = "date";
+  public static final String EMAIL_ATTACHMENT_ENCODING = "encoding of attachment";
+  public static final String EMAIL_ATTACHMENT_MIMETYPE = "mime type of attachment";
+  public static final String EMAIL_VERSION = "1.0";
+  public static final String MIMETYPE_TEXT_PLAIN = "text/plain";
+  public static final String MIMETYPE_HTML = "text/html";
+  public static final String ENCODING_FIELD = "encoding";
+  public static final String MIMETYPE_FIELD = "mimetype";
+  protected final static long SESSION_EXPIRATION_MILLISECONDS = 300000L;
+  //public static final String TO = "To";
+}
---
+++ connectors/email/connector/src/main/resources/org/apache/manifoldcf/crawler/connectors/email/ConfigurationHeader.js
@@ -0,0 +1,67 @@
+<!--
+ Licensed to the Apache Software Foundation (ASF) under one or more
+ contributor license agreements. See the NOTICE file distributed with
+ this work for additional information regarding copyright ownership.
+ The ASF licenses this file to You under the Apache License, Version 2.0
+ (the "License"); you may not use this file except in compliance with
+ the License. You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+-->
+
+<script type="text/javascript">
+<!--
+function checkConfigForSave()
+    {
+      if (editconnection.server.value == "")
+      {
+        alert("Enter a email server url");
+        SelectTab("Server");
+        editconnection.server.focus();
+        return false;
+      }
+      return true;
+    }
+
+    function addProperty()
+        {
+          postFormSetAnchor("property"); //Repost the form and send the browser to property anchor
+        }
+
+    function SpecOp(n, opValue, anchorvalue)
+        {
+          eval("editconnection."+n+".value = \""+opValue+"\"");
+          postFormSetAnchor(anchorvalue);
+        }
+
+
+
+    function FindDelete(n)
+        {
+          SpecOp("findop_"+n, "Delete", "find_"+n);
+        }
+
+    function FindAdd(n)
+        {
+          if (editconnection.findname.value == "")
+          {
+            alert("Please select a metadata name first.");
+            editconnection.findname.focus();
+            return;
+          }
+          if (editconnection.findvalue.value == "")
+          {
+            alert("value cannot be blank.");
+            editconnection.findvalue.focus();
+            return;
+          }
+          SpecOp("findop", "Add", "find_"+n);
+        }
+//-->
+</script>
