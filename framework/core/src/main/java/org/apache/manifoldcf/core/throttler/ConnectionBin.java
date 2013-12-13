50da9f287efcf2d9be0523ddcce467e025807444
Karl Wright <kwright@apache.org>
2013-12-13 01:30:10 +0000

    Create local bins for connection allocation, fetch rate waiting, and byte rate waiting.
    git-svn-id: https://svn.apache.org/repos/asf/manifoldcf/branches/CONNECTORS-829@1550611 13f79535-47bb-0310-9956-ffa450edef68

--- framework/core/src/main/java/org/apache/manifoldcf/core/throttler/ConnectionBin.java
+++ framework/core/src/main/java/org/apache/manifoldcf/core/throttler/ConnectionBin.java
@@ -28,20 +28,29 @@ import org.apache.manifoldcf.core.system.ManifoldCF;
 * connections, and pooling established connections, is functionality that must reside in the
 * caller.
 *
+* The 'connections' each connection bin tracks are connections outstanding that share this bin name.
+* Not all such connections are identical; some may in fact have entirely different sets of
+* bins associated with them, but they all have the specific bin in common.  Since each bin has its
+* own unique limit, this effectively means that in order to get a connection, you need to find an
+* available slot in ALL of its constituent connection bins.  If the connections are pooled, it makes
+* the most sense to divide the pool up by characteristics such that identical connections are all
+* handled together - and it is reasonable to presume that an identical connection has identical
+* connection bins.
+*
 * NOTE WELL: This is entirely local in operation
 */
 public class ConnectionBin
 {
   /** This is the bin name which this connection pool belongs to */
   protected final String binName;
-  /** This is the number of connections in this bin that are signed out and presumably in use */
+  /** This is the maximum number of active connections allowed for this bin */
+  protected int maxActiveConnections = 0;
+  /** This is the number of connections in this bin that have been reserved - that is, they
+  * are promised to various callers, but those callers have not yet committed to obtaining them. */
+  protected int reservedConnections = 0;
+  /** This is the number of connections in this bin that are connected; immaterial whether they are
+  * in use or in a pool somewhere. */
   protected int inUseConnections = 0;
-  /** This is the last time a fetch was done on this bin */
-  protected long lastFetchTime = 0L;
-  /** This object is what we synchronize on when we are waiting on a connection to free up for this
-  * bin.  This is a separate object, because we also want to protect the integrity of the
-  * ConnectionBin object itself, for which we'll use the ConnectionBin's synchronizer. */
-  protected final Integer connectionWait = new Integer(0);
 
   /** Constructor. */
   public ConnectionBin(String binName)
@@ -55,12 +64,56 @@ public class ConnectionBin
     return binName;
   }
 
-  /** Note the creation of an active connection that belongs to this bin.  The slots all must
+  /** Update the maximum number of active connections.
+  */
+  public synchronized void updateMaxActiveConnections(int maxActiveConnections)
+  {
+    // Update the number and wake up any waiting threads; they will take care of everything.
+    this.maxActiveConnections = maxActiveConnections;
+    notifyAll();
+  }
+
+  /** Reserve a connection from this bin.  If there is no connection yet available to reserve, wait
+  * until there is. */
+  public synchronized void reserveAConnection()
+    throws InterruptedException
+  {
+    // Reserved connections keep a slot available which can't be used by anyone else.
+    // Connection bins are always sorted so that deadlocks can't occur.
+    // Once all slots are reserved, the caller will go ahead and create the necessary connection
+    // and convert the reservation to a new connection.
+    while (true)
+    {
+      if (inUseConnections + reservedConnections < maxActiveConnections)
+      {
+        reservedConnections++;
+        return;
+      }
+      // Wait for a connection to free up.  Note that it is up to the caller to free stuff up.
+      wait();
+    }
+  }
+  
+  /** Clear reservation.
+  */
+  public synchronized void clearReservation()
+  {
+    if (reservedConnections == 0)
+      throw new IllegalStateException("Can't clear a reservation we don't have");
+    reservedConnections--;
+    notifyAll();
+  }
+  
+  /** Note the creation of an active connection that belongs to this bin.  The connection MUST
   * have been reserved prior to the connection being created.
   */
   public synchronized void noteConnectionCreation()
   {
+    if (reservedConnections == 0)
+      throw new IllegalStateException("Creating a connection when no connection slot reserved!");
+    reservedConnections--;
     inUseConnections++;
+    // No notification needed because the total number of reserved+active connections did not change.
   }
 
   /** Note the destruction of an active connection that belongs to this bin.
@@ -68,26 +121,10 @@ public class ConnectionBin
   public synchronized void noteConnectionDestruction()
   {
     inUseConnections--;
+    notifyAll();
   }
 
-  /** Note a new time for connection fetch for this pool.
-  *@param currentTime is the time the fetch was started.
-  */
-  public synchronized void setLastFetchTime(long currentTime)
-  {
-    if (currentTime > lastFetchTime)
-      lastFetchTime = currentTime;
-  }
-
-  /** Get the last fetch time.
-  *@return the time.
-  */
-  public synchronized long getLastFetchTime()
-  {
-    return lastFetchTime;
-  }
-
-  /** Count connections that are in use.
+  /** Count connections that are active.
   *@return connections that are in use.
   */
   public synchronized int countConnections()
