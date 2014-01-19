227e5a49a6d2ff7ae53fdfbff5707198c025db65
Karl Wright <kwright@apache.org>
2014-01-19 15:46:50 +0000

    Pull up fix for CONNECTORS-862 from trunk.
    git-svn-id: https://svn.apache.org/repos/asf/manifoldcf/branches/release-1.5-branch@1559515 13f79535-47bb-0310-9956-ffa450edef68

--- framework/example-multiprocess-zk-common/runzookeeper.sh
+++ framework/example-multiprocess-zk-common/runzookeeper.sh
@@ -18,9 +18,7 @@
 #Make sure environment variables are properly set
 if [ -e "$JAVA_HOME"/bin/java ] ; then
     if [ -f ./properties.xml ] ; then
-        # Set the MCF_HOME variable
-        export MCF_HOME=$PWD
-        processes/executecommand.sh org.apache.zookeeper.server.quorum.QuorumPeerMain zookeeper.cfg
+        ./executecommand.sh org.apache.zookeeper.server.quorum.QuorumPeerMain zookeeper.cfg
         exit $?
         
     else
