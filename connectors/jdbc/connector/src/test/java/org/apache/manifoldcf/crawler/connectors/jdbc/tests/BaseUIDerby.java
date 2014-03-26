09219a15e628bd0ee2c720340648dd5694a6d1a5
Karl Wright <kwright@apache.org>
2014-03-26 18:44:17 +0000

    Move jdbc tests to connector
    git-svn-id: https://svn.apache.org/repos/asf/manifoldcf/branches/CONNECTORS-912@1582006 13f79535-47bb-0310-9956-ffa450edef68

--- connectors/jdbc/connector/src/test/java/org/apache/manifoldcf/crawler/connectors/jdbc/tests/BaseUIDerby.java
+++ connectors/jdbc/connector/src/test/java/org/apache/manifoldcf/crawler/connectors/jdbc/tests/BaseUIDerby.java
@@ -16,7 +16,7 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-package org.apache.manifoldcf.jdbc_tests;
+package org.apache.manifoldcf.crawler.connectors.jdbc.tests;
 
 import org.apache.manifoldcf.core.interfaces.*;
 import org.apache.manifoldcf.agents.interfaces.*;
@@ -51,7 +51,7 @@ public class BaseUIDerby extends org.apache.manifoldcf.crawler.tests.ConnectorBa
   @Override
   protected String[] getOutputClasses()
   {
-    return new String[]{"org.apache.manifoldcf.agents.output.nullconnector.NullConnector"};
+    return new String[]{"org.apache.manifoldcf.agents.tests.TestingOutputConnector"};
   }
 
   @Override
