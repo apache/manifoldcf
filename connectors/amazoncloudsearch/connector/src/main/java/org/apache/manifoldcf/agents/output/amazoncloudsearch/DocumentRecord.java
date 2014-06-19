35a8a9cab0d067d6b8c3168e82de0c9aaeeaa037
Karl Wright <kwright@apache.org>
2014-06-19 22:24:20 +0000

    Revamp DocumentChunkManager; this breaks the build
    git-svn-id: https://svn.apache.org/repos/asf/manifoldcf/branches/CONNECTORS-954@1604042 13f79535-47bb-0310-9956-ffa450edef68

---
+++ connectors/amazoncloudsearch/connector/src/main/java/org/apache/manifoldcf/agents/output/amazoncloudsearch/DocumentRecord.java
@@ -0,0 +1,85 @@
+/* $Id$ */
+
+/**
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements. See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* The ASF licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License. You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package org.apache.manifoldcf.agents.output.amazoncloudsearch;
+
+import org.apache.manifoldcf.core.*;
+
+import java.io.*;
+
+public class DocumentRecord {
+  
+  protected final String host;
+  protected final String path;
+  protected final String uid;
+  protected final boolean delete;
+  protected final BinaryInput data;
+  
+  public DocumentRecord(String host, String path, String uid, boolean delete, BinaryInput data)
+  {
+    this.host = host;
+    this.path = path;
+    this.uid = uid;
+    this.delete = delete;
+    this.data = data;
+  }
+
+  public String getHost()
+  {
+    return host;
+  }
+  
+  public String getPath()
+  {
+    return path;
+  }
+  
+  public String getUid()
+  {
+    return uid;
+  }
+  
+  public boolean getDelete()
+  {
+    return delete;
+  }
+  
+  public long getStreamLength()
+    throws ManifoldCFException
+  {
+    if (data != null)
+      return data.getLength();
+    return 0L;
+  }
+  
+  public InputStream getDataStream()
+    throws ManifoldCFException
+  {
+    if (data != null)
+      return data.getStream();
+    return null;
+  }
+  
+  public void close()
+    throws ManifoldCFException
+  {
+    if (data != null)
+      data.discard();
+  }
+  
+}
