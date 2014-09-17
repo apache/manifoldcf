fb22b8967f31d98c48be27166b1ceee57ac1a9ac
Karl Wright <kwright@apache.org>
2014-09-17 08:20:57 +0000

    Pull up output connector-related changes from CONNECTORS-977 on trunk.
    git-svn-id: https://svn.apache.org/repos/asf/manifoldcf/branches/dev_1x@1625484 13f79535-47bb-0310-9956-ffa450edef68

--- connectors/nulloutput/connector/src/main/java/org/apache/manifoldcf/agents/output/nullconnector/NullConnector.java
+++ connectors/nulloutput/connector/src/main/java/org/apache/manifoldcf/agents/output/nullconnector/NullConnector.java
@@ -168,24 +168,4 @@ public class NullConnector extends org.apache.manifoldcf.agents.output.BaseOutpu
     activities.recordActivity(null,JOB_COMPLETE_ACTIVITY,null,"","OK",null);
   }
 
-  /** Obtain the name of the form check javascript method to call.
-  *@param connectionSequenceNumber is the unique number of this connection within the job.
-  *@return the name of the form check javascript method.
-  */
-  @Override
-  public String getFormCheckJavascriptMethodName(int connectionSequenceNumber)
-  {
-    return "s"+connectionSequenceNumber+"_checkSpecification";
-  }
-
-  /** Obtain the name of the form presave check javascript method to call.
-  *@param connectionSequenceNumber is the unique number of this connection within the job.
-  *@return the name of the form presave check javascript method.
-  */
-  @Override
-  public String getFormPresaveCheckJavascriptMethodName(int connectionSequenceNumber)
-  {
-    return "s"+connectionSequenceNumber+"_checkSpecificationForSave";
-  }
-
 }
