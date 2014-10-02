4b2e43e2f13856d225f16b3c12fd2731fa868d22
Karl Wright <kwright@apache.org>
2014-10-02 22:43:47 +0000

    More rearrangement
    git-svn-id: https://svn.apache.org/repos/asf/manifoldcf/branches/CONNECTORS-1037@1629092 13f79535-47bb-0310-9956-ffa450edef68

--- connectors/alfresco-webscript/connector/src/main/java/org/apache/manifoldcf/authorities/authorities/alfrescowebscript/ConfigurationHandler.java
+++ connectors/alfresco-webscript/connector/src/main/java/org/apache/manifoldcf/authorities/authorities/alfrescowebscript/ConfigurationHandler.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.manifoldcf.authorities.authorities.alfresco;
+package org.apache.manifoldcf.authorities.authorities.alfrescowebscript;
 
 import java.io.IOException;
 import java.io.InputStream;
