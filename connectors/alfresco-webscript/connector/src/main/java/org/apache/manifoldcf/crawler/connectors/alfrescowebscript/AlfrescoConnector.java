c2a36e5683b69a29c477425ee2542bff7abd9f76
Karl Wright <kwright@apache.org>
2014-10-02 22:36:08 +0000

    Move connector-side packages around
    git-svn-id: https://svn.apache.org/repos/asf/manifoldcf/branches/CONNECTORS-1037@1629090 13f79535-47bb-0310-9956-ffa450edef68

--- connectors/alfresco-webscript/connector/src/main/java/org/apache/manifoldcf/crawler/connectors/alfrescowebscript/AlfrescoConnector.java
+++ connectors/alfresco-webscript/connector/src/main/java/org/apache/manifoldcf/crawler/connectors/alfrescowebscript/AlfrescoConnector.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.manifoldcf.crawler.connectors.alfresco.webscript;
+package org.apache.manifoldcf.crawler.connectors.alfrescowebscript;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
