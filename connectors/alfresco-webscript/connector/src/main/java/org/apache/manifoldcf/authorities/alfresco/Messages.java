303072b6fdb5ab95fa030da4e71319099ef50ab8
Karl Wright <kwright@apache.org>
2014-10-02 22:15:32 +0000

    Coding standards for Messages and ConfigurationHandler classes
    git-svn-id: https://svn.apache.org/repos/asf/manifoldcf/branches/CONNECTORS-1037@1629085 13f79535-47bb-0310-9956-ffa450edef68

--- connectors/alfresco-webscript/connector/src/main/java/org/apache/manifoldcf/authorities/alfresco/Messages.java
+++ connectors/alfresco-webscript/connector/src/main/java/org/apache/manifoldcf/authorities/alfresco/Messages.java
@@ -34,79 +34,79 @@ public class Messages extends org.apache.manifoldcf.ui.i18n.Messages
 
     public static String getString(Locale locale, String messageKey)
     {
-        return getString(DEFAULT_BUNDLE_NAME, locale, messageKey, null);
+      return getString(DEFAULT_BUNDLE_NAME, locale, messageKey, null);
     }
 
     public static String getAttributeString(Locale locale, String messageKey)
     {
-        return getAttributeString(DEFAULT_BUNDLE_NAME, locale, messageKey, null);
+      return getAttributeString(DEFAULT_BUNDLE_NAME, locale, messageKey, null);
     }
 
     public static String getBodyString(Locale locale, String messageKey)
     {
-        return getBodyString(DEFAULT_BUNDLE_NAME, locale, messageKey, null);
+      return getBodyString(DEFAULT_BUNDLE_NAME, locale, messageKey, null);
     }
 
     public static String getAttributeJavascriptString(Locale locale, String messageKey)
     {
-        return getAttributeJavascriptString(DEFAULT_BUNDLE_NAME, locale, messageKey, null);
+      return getAttributeJavascriptString(DEFAULT_BUNDLE_NAME, locale, messageKey, null);
     }
 
     public static String getBodyJavascriptString(Locale locale, String messageKey)
     {
-        return getBodyJavascriptString(DEFAULT_BUNDLE_NAME, locale, messageKey, null);
+      return getBodyJavascriptString(DEFAULT_BUNDLE_NAME, locale, messageKey, null);
     }
 
     public static String getString(Locale locale, String messageKey, Object[] args)
     {
-        return getString(DEFAULT_BUNDLE_NAME, locale, messageKey, args);
+      return getString(DEFAULT_BUNDLE_NAME, locale, messageKey, args);
     }
 
     public static String getAttributeString(Locale locale, String messageKey, Object[] args)
     {
-        return getAttributeString(DEFAULT_BUNDLE_NAME, locale, messageKey, args);
+      return getAttributeString(DEFAULT_BUNDLE_NAME, locale, messageKey, args);
     }
 
     public static String getBodyString(Locale locale, String messageKey, Object[] args)
     {
-        return getBodyString(DEFAULT_BUNDLE_NAME, locale, messageKey, args);
+      return getBodyString(DEFAULT_BUNDLE_NAME, locale, messageKey, args);
     }
 
     public static String getAttributeJavascriptString(Locale locale, String messageKey, Object[] args)
     {
-        return getAttributeJavascriptString(DEFAULT_BUNDLE_NAME, locale, messageKey, args);
+      return getAttributeJavascriptString(DEFAULT_BUNDLE_NAME, locale, messageKey, args);
     }
 
     public static String getBodyJavascriptString(Locale locale, String messageKey, Object[] args)
     {
-        return getBodyJavascriptString(DEFAULT_BUNDLE_NAME, locale, messageKey, args);
+      return getBodyJavascriptString(DEFAULT_BUNDLE_NAME, locale, messageKey, args);
     }
 
     // More general methods which allow bundlenames and class loaders to be specified.
 
     public static String getString(String bundleName, Locale locale, String messageKey, Object[] args)
     {
-        return getString(Messages.class, bundleName, locale, messageKey, args);
+      return getString(Messages.class, bundleName, locale, messageKey, args);
     }
 
     public static String getAttributeString(String bundleName, Locale locale, String messageKey, Object[] args)
     {
-        return getAttributeString(Messages.class, bundleName, locale, messageKey, args);
+      return getAttributeString(Messages.class, bundleName, locale, messageKey, args);
     }
 
     public static String getBodyString(String bundleName, Locale locale, String messageKey, Object[] args)
     {
-        return getBodyString(Messages.class, bundleName, locale, messageKey, args);
+      return getBodyString(Messages.class, bundleName, locale, messageKey, args);
     }
 
     public static String getAttributeJavascriptString(String bundleName, Locale locale, String messageKey, Object[] args)
     {
-        return getAttributeJavascriptString(Messages.class, bundleName, locale, messageKey, args);
+      return getAttributeJavascriptString(Messages.class, bundleName, locale, messageKey, args);
     }
 
     public static String getBodyJavascriptString(String bundleName, Locale locale, String messageKey, Object[] args)
     {
-        return getBodyJavascriptString(Messages.class, bundleName, locale, messageKey, args);
+      return getBodyJavascriptString(Messages.class, bundleName, locale, messageKey, args);
     }
 
     // Resource output
@@ -115,23 +115,23 @@ public class Messages extends org.apache.manifoldcf.ui.i18n.Messages
                                       Map<String,String> substitutionParameters, boolean mapToUpperCase)
             throws ManifoldCFException
     {
-        outputResource(output,Messages.class,DEFAULT_PATH_NAME,locale,resourceKey,
+      outputResource(output,Messages.class,DEFAULT_PATH_NAME,locale,resourceKey,
                 substitutionParameters,mapToUpperCase);
     }
 
     public static void outputResourceWithVelocity(IHTTPOutput output, Locale locale, String resourceKey,
                                                   Map<String,String> substitutionParameters, boolean mapToUpperCase)
-            throws ManifoldCFException
+      throws ManifoldCFException
     {
-        outputResourceWithVelocity(output,Messages.class,DEFAULT_BUNDLE_NAME,DEFAULT_PATH_NAME,locale,resourceKey,
+      outputResourceWithVelocity(output,Messages.class,DEFAULT_BUNDLE_NAME,DEFAULT_PATH_NAME,locale,resourceKey,
                 substitutionParameters,mapToUpperCase);
     }
 
     public static void outputResourceWithVelocity(IHTTPOutput output, Locale locale, String resourceKey,
                                                   Map<String,Object> contextObjects)
-            throws ManifoldCFException
+      throws ManifoldCFException
     {
-        outputResourceWithVelocity(output,Messages.class,DEFAULT_BUNDLE_NAME,DEFAULT_PATH_NAME,locale,resourceKey,
+      outputResourceWithVelocity(output,Messages.class,DEFAULT_BUNDLE_NAME,DEFAULT_PATH_NAME,locale,resourceKey,
                 contextObjects);
     }
 
