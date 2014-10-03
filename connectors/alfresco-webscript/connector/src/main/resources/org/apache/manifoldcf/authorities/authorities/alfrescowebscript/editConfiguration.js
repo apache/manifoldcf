4aa9f967d907914f5d7c277c0d493662f5666e14
Karl Wright <kwright@apache.org>
2014-10-03 12:14:05 +0000

    Properly escape UI for authority
    git-svn-id: https://svn.apache.org/repos/asf/manifoldcf/branches/CONNECTORS-1037@1629188 13f79535-47bb-0310-9956-ffa450edef68

--- connectors/alfresco-webscript/connector/src/main/resources/org/apache/manifoldcf/authorities/authorities/alfrescowebscript/editConfiguration.js
+++ connectors/alfresco-webscript/connector/src/main/resources/org/apache/manifoldcf/authorities/authorities/alfrescowebscript/editConfiguration.js
@@ -1,29 +1,29 @@
 <script type="text/javascript">
 <!--
 function checkConfig() {
-	return true;
+  return true;
 }
 
 function checkConfigForSave() {
-	if (editconnection.protocol.value == "") {
-		alert("Protocol must not be empty!");
-		SelectTab("Server");
-		editconnection.protocol.focus();
-		return false;
-	}
-	if (editconnection.hostname.value == "") {
-		alert("Hostname must not be empty!");
-		SelectTab("Server");
-		editconnection.hostname.focus();
-		return false;
-	}
-	if (editconnection.endpoint.value == "") {
-		alert("Endpoint must not be empty!");
-		SelectTab("Server");
-		editconnection.endpoint.focus();
-		return false;
-	}
-	return true;
+  if (editconnection.protocol.value == "") {
+    alert("Protocol must not be empty!");
+    SelectTab("Server");
+    editconnection.protocol.focus();
+    return false;
+  }
+  if (editconnection.hostname.value == "") {
+    alert("Hostname must not be empty!");
+    SelectTab("Server");
+    editconnection.hostname.focus();
+    return false;
+  }
+  if (editconnection.endpoint.value == "") {
+    alert("Endpoint must not be empty!");
+    SelectTab("Server");
+    editconnection.endpoint.focus();
+    return false;
+  }
+  return true;
 }
 // -->
 </script>
