32c0bc803b72d9aa1f79244420f942b4046f9ea4
Karl Wright <kwright@apache.org>
2014-10-06 16:34:13 +0000

    Change package of documentum authority.  Part of CONNECTORS-1065.
    git-svn-id: https://svn.apache.org/repos/asf/manifoldcf/trunk@1629703 13f79535-47bb-0310-9956-ffa450edef68

--- connectors/documentum/connector/src/main/java/org/apache/manifoldcf/authorities/authorities/DCTM/AuthorityConnector.java
+++ connectors/documentum/connector/src/main/java/org/apache/manifoldcf/authorities/authorities/DCTM/AuthorityConnector.java
@@ -16,7 +16,7 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-package org.apache.manifoldcf.crawler.authorities.DCTM;
+package org.apache.manifoldcf.authorities.authorities.DCTM;
 
 import org.apache.log4j.*;
 import org.apache.manifoldcf.core.interfaces.*;
--- connectors/documentum/connector/src/main/java/org/apache/manifoldcf/authorities/authorities/DCTM/Messages.java
+++ connectors/documentum/connector/src/main/java/org/apache/manifoldcf/authorities/authorities/DCTM/Messages.java
@@ -16,7 +16,7 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-package org.apache.manifoldcf.crawler.authorities.DCTM;
+package org.apache.manifoldcf.authorities.authorities.DCTM;
 
 import java.util.Locale;
 import java.util.Map;
@@ -25,8 +25,8 @@ import org.apache.manifoldcf.core.interfaces.IHTTPOutput;
 
 public class Messages extends org.apache.manifoldcf.ui.i18n.Messages
 {
-  public static final String DEFAULT_BUNDLE_NAME="org.apache.manifoldcf.crawler.authorities.DCTM.common";
-  public static final String DEFAULT_PATH_NAME="org.apache.manifoldcf.crawler.authorities.DCTM";
+  public static final String DEFAULT_BUNDLE_NAME="org.apache.manifoldcf.authorities.authorities.DCTM.common";
+  public static final String DEFAULT_PATH_NAME="org.apache.manifoldcf.authorities.authorities.DCTM";
   
   /** Constructor - do no instantiate
   */
