33542f6b7c9d8019cff2f9a71ced7b9ae01e6479
Karl Wright <kwright@apache.org>
2014-10-11 12:18:41 +0000

    Pull up fix for CONNECTORS-1061 from trunk.
    git-svn-id: https://svn.apache.org/repos/asf/manifoldcf/branches/dev_1x@1631040 13f79535-47bb-0310-9956-ffa450edef68

--- connectors/alfresco-webscript/connector/src/main/resources/org/apache/manifoldcf/crawler/connectors/alfrescowebscript/editConfiguration.js
+++ connectors/alfresco-webscript/connector/src/main/resources/org/apache/manifoldcf/crawler/connectors/alfrescowebscript/editConfiguration.js
@@ -1,3 +1,20 @@
+<!--
+ Licensed to the Apache Software Foundation (ASF) under one or more
+ contributor license agreements.  See the NOTICE file distributed with
+ this work for additional information regarding copyright ownership.
+ The ASF licenses this file to You under the Apache License, Version 2.0
+ (the "License"); you may not use this file except in compliance with
+ the License.  You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+-->
+
 <script type="text/javascript">
 <!--
 function checkConfig() {
@@ -5,12 +22,6 @@ function checkConfig() {
 }
 
 function checkConfigForSave() {
-  if (editconnection.protocol.value == "") {
-    alert("$Encoder.bodyJavascriptEscape($ResourceBundle.getString('Alfresco.ProtocolMustNotBeEmpty'))");
-    SelectTab("$Encoder.bodyJavascriptEscape($ResourceBundle.getString('Alfresco.Server'))");
-    editconnection.protocol.focus();
-    return false;
-  }
   if (editconnection.hostname.value == "") {
     alert("$Encoder.bodyJavascriptEscape($ResourceBundle.getString('Alfresco.HostnameMustNotBeEmpty'))");
     SelectTab("$Encoder.bodyJavascriptEscape($ResourceBundle.getString('Alfresco.Server'))");
@@ -21,12 +32,6 @@ function checkConfigForSave() {
     alert("$Encoder.bodyJavascriptEscape($ResourceBundle.getString('Alfresco.EndpointMustNotBeEmpty'))");
     SelectTab("$Encoder.bodyJavascriptEscape($ResourceBundle.getString('Alfresco.Server'))");
     editconnection.endpoint.focus();
-    return false;
-  }
-  if (editconnection.storeprotocol.value == "") {
-    alert("$Encoder.bodyJavascriptEscape($ResourceBundle.getString('Alfresco.StoreProtocolMustNotBeEmpty'))");
-    SelectTab("$Encoder.bodyJavascriptEscape($ResourceBundle.getString('Alfresco.Server'))");
-    editconnection.storeprotocol.focus();
     return false;
   }
   if (editconnection.storeid.value == "") {
