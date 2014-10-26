4b8004f4fc228fb2c4cd191b8594882415439dce
Karl Wright <kwright@apache.org>
2014-10-26 10:51:45 +0000

    Change packages of test classes
    git-svn-id: https://svn.apache.org/repos/asf/manifoldcf/branches/CONNECTORS-1085@1634298 13f79535-47bb-0310-9956-ffa450edef68

--- framework/connector-common/src/test/java/org/apache/manifoldcf/connectorcommon/jsongen/TestJsonGen.java
+++ framework/connector-common/src/test/java/org/apache/manifoldcf/connectorcommon/jsongen/TestJsonGen.java
@@ -16,7 +16,7 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-package org.apache.manifoldcf.core.jsongen;
+package org.apache.manifoldcf.connectorcommon.jsongen;
 
 import java.io.*;
 import org.junit.*;
