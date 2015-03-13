2ce9cc8085686c1a3316948a88d9f519ac511388
Rafa Haro <rharo@apache.org>
2015-03-13 08:26:02 +0000

    Updated SearchBlox Document Test and Fixed Bug for deletion
    git-svn-id: https://svn.apache.org/repos/asf/manifoldcf/branches/CONNECTORS-1168@1666371 13f79535-47bb-0310-9956-ffa450edef68

--- connectors/searchblox/connector/src/test/java/org/apache/manifoldcf/agents/output/searchblox/tests/SearchBloxDocumentTest.java
+++ connectors/searchblox/connector/src/test/java/org/apache/manifoldcf/agents/output/searchblox/tests/SearchBloxDocumentTest.java
@@ -26,6 +26,7 @@ import org.apache.manifoldcf.agents.output.searchblox.SearchBloxDocument;
 import org.apache.manifoldcf.agents.output.searchblox.SearchBloxDocument.DocumentAction;
 import org.apache.manifoldcf.agents.output.searchblox.SearchBloxDocument.IndexingFormat;
 import org.apache.manifoldcf.agents.output.searchblox.SearchBloxException;
+import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
@@ -72,14 +73,41 @@ public class SearchBloxDocumentTest extends TestCase{
         realRepoDoc.addField("content","I am a nice content in english!");
         realRepoDoc.addField("description","I am a little tiny description");
         realRepoDoc.addField("meta1","I am META1!");
+        realRepoDoc.addField("meta2","I am META2!");
         realRepoDoc.setMimeType("html");
         realRepoDoc.setBinary(stream,100);
         realRepoDoc.setCreatedDate(new Date(System.currentTimeMillis()));
+        realRepoDoc.setSecurityACL(RepositoryDocument.SECURITY_TYPE_SHARE,new String[]{"user1","user2","user3"});
+        realRepoDoc.setSecurityACL(RepositoryDocument.SECURITY_TYPE_DOCUMENT,new String[]{"user12","user22","user33"});
+        realRepoDoc.setSecurityDenyACL(RepositoryDocument.SECURITY_TYPE_SHARE, new String[]{"user4", "user5"});
+        realRepoDoc.setSecurityDenyACL(RepositoryDocument.SECURITY_TYPE_DOCUMENT, new String[]{"user42", "user52"});
+        //allowAttributeName + aclType
         return realRepoDoc;
     }
 
-    public void testXmlString() throws SearchBloxException {
+    @Test
+    public void testUpdateXmlString() throws SearchBloxException {
         String xmlGenerated=toTest.toString(IndexingFormat.XML, DocumentAction.ADD_UPDATE);
+        System.out.println(xmlGenerated);
+        String xmlExpected="<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+                "<searchblox apikey=\"apikey\"><document colname=\"collection1\">" +
+                "<uid>URI</uid><title boost=\"1\">I am a nice title</title><content boost=\"2\">I am a nice content in english!</content>" +
+                "<description boost=\"4\">I am a little tiny description</description><size>100</size><contenttype>html</contenttype>" +
+                "<meta name=\"meta2\">I am META2!</meta><meta name=\"share_allow\">user3</meta>" +
+                "<meta name=\"share_allow\">user2</meta><meta name=\"share_allow\">user1</meta>" +
+                "<meta name=\"meta1\">I am META1!</meta><meta name=\"share_deny\">user4</meta>" +
+                "<meta name=\"share_deny\">user5</meta><meta name=\"document_deny\">user52</meta>" +
+                "<meta name=\"document_deny\">user42</meta><meta name=\"document_allow\">user22</meta>" +
+                "<meta name=\"document_allow\">user12</meta><meta name=\"document_allow\">user33</meta></document></searchblox>";
+        assertEquals(xmlExpected,xmlGenerated);
+    }
+
+    @Test
+    public void testDeleteXmlString() throws SearchBloxException {
+        String xmlGenerated=toTest.toString(IndexingFormat.XML, DocumentAction.DELETE);
+        System.out.println(xmlGenerated);
+        String xmlExpected="<?xml version=\"1.0\" encoding=\"UTF-8\"?><searchblox apikey=\"apikey\"><document colname=\"collection1\" uid=\"URI\"/></searchblox>";
+        assertEquals(xmlExpected,xmlGenerated);
     }
 
 }
