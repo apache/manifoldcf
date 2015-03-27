2554a93939ec447965be749a37eddc3a50bb2a9d
Karl Wright <kwright@apache.org>
2015-03-27 19:33:37 +0000

    Fix for CONNECTORS-1176.
    git-svn-id: https://svn.apache.org/repos/asf/manifoldcf/trunk@1669660 13f79535-47bb-0310-9956-ffa450edef68

--- connectors/solr/connector/src/main/java/org/apache/manifoldcf/agents/output/solr/HttpPoster.java
+++ connectors/solr/connector/src/main/java/org/apache/manifoldcf/agents/output/solr/HttpPoster.java
@@ -43,9 +43,9 @@ import org.apache.http.auth.Credentials;
 import org.apache.http.auth.UsernamePasswordCredentials;
 
 import org.apache.manifoldcf.core.util.URLEncoder;
-import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.client.solrj.impl.CloudSolrServer;
+import org.apache.solr.client.solrj.impl.CloudSolrClient;
 import org.apache.solr.client.solrj.request.ContentStreamUpdateRequest;
 import org.apache.solr.client.solrj.request.UpdateRequest;
 import org.apache.solr.client.solrj.impl.XMLResponseParser;
@@ -83,7 +83,7 @@ public class HttpPoster
 
   // Solrj connection-associated objects
   protected PoolingHttpClientConnectionManager connectionManager = null;
-  protected SolrServer solrServer = null;
+  protected SolrClient solrServer = null;
   
   // Action URI pieces
   private final String postUpdateAction;
@@ -153,7 +153,7 @@ public class HttpPoster
     
     try
     {
-      CloudSolrServer cloudSolrServer = new CloudSolrServer(zookeeperHosts, new ModifiedLBHttpSolrServer(HttpClientUtil.createClient(null)));
+      CloudSolrClient cloudSolrServer = new CloudSolrClient(zookeeperHosts, new ModifiedLBHttpSolrClient(HttpClientUtil.createClient(null)));
       cloudSolrServer.setZkClientTimeout(zkClientTimeout);
       cloudSolrServer.setZkConnectTimeout(zkConnectTimeout);
       cloudSolrServer.setDefaultCollection(collection);
@@ -264,7 +264,7 @@ public class HttpPoster
 
 
     String httpSolrServerUrl = protocol + "://" + server + ":" + port + location;
-    solrServer = new ModifiedHttpSolrServer(httpSolrServerUrl, localClient, new XMLResponseParser());
+    solrServer = new ModifiedHttpSolrClient(httpSolrServerUrl, localClient, new XMLResponseParser());
   }
 
   /** Shut down the poster.
@@ -1556,7 +1556,7 @@ public class HttpPoster
     }
 
     @Override
-    public SolrPingResponse process( SolrServer server ) throws SolrServerException, IOException 
+    public SolrPingResponse process( SolrClient server ) throws SolrServerException, IOException 
     {
       long startTime = System.currentTimeMillis();
       SolrPingResponse res = new SolrPingResponse();
