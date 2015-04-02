1d0440b5dd0867bd8bbd6f6e7862f4b5b6a8d33e
Karl Wright <kwright@apache.org>
2015-04-02 07:14:08 +0000

    Pull up SearchBlox connector from trunk.
    git-svn-id: https://svn.apache.org/repos/asf/manifoldcf/branches/dev_1x@1670830 13f79535-47bb-0310-9956-ffa450edef68

---
+++ connectors/searchblox/connector/src/main/java/org/apache/manifoldcf/agents/output/searchblox/SearchBloxConfig.java
@@ -0,0 +1,21 @@
+package org.apache.manifoldcf.agents.output.searchblox;
+
+/**
+ * <p>SearchBloxConfig class</p>
+ * @author Antonio David Perez Morales <adperezmorales@apache.org>
+ * 
+ */
+class SearchBloxConfig {
+
+	static final String NODE_CONFIGURATION = "configuration";
+	static final String ATTRIBUTE_TITLEBOOST = "title_boost";
+	static final String ATTRIBUTE_CONTENTBOOST = "content_boost";
+	static final String ATTRIBUTE_KEYWORDSBOOST = "keywords_boost";
+	static final String ATTRIBUTE_DESCRIPTIONBOOST = "description_boost";
+	static final String ATTRIBUTE_POOLSIZE = "poolsize";
+	static final String ATTRIBUTE_TIMEOUT_CONNECTION = "timeoutconnection";
+	static final String ATTRIBUTE_TIMEOUT_SOCKET = "timeoutsocket";
+	static final String ATTRIBUTE_INDEX_FORMAT = "indexformat";
+	static final String ATTRIBUTE_COLLECTION_NAME = "collection";
+
+}
