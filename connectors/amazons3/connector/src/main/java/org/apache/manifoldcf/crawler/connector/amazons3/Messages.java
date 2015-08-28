d37e695246a48fcc534862ef64e236c60e8eaf9f
Karl Wright <kwright@apache.org>
2015-08-28 15:13:30 +0000

    Package names should be standard
    git-svn-id: https://svn.apache.org/repos/asf/manifoldcf/branches/CONNECTORS-1233@1698350 13f79535-47bb-0310-9956-ffa450edef68

--- connectors/amazons3/connector/src/main/java/org/apache/manifoldcf/crawler/connector/amazons3/Messages.java
+++ connectors/amazons3/connector/src/main/java/org/apache/manifoldcf/crawler/connector/amazons3/Messages.java
@@ -14,7 +14,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package org.apache.manifoldcf.crawler.connectors.amazons3;
+package org.apache.manifoldcf.crawler.connector.amazons3;
 
 import java.util.Locale;
 import java.util.Map;
