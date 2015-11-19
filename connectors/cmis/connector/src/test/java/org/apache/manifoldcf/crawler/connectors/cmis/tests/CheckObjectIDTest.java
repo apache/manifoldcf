4eaf317999bb6ff893dd2c093eac60c8b8effc82
Shinichiro Abe <shinichiro@apache.org>
2015-11-19 07:00:44 +0000

    Fix for CONNECTORS-1255
    git-svn-id: https://svn.apache.org/repos/asf/manifoldcf/trunk@1715112 13f79535-47bb-0310-9956-ffa450edef68

--- connectors/cmis/connector/src/test/java/org/apache/manifoldcf/crawler/connectors/cmis/tests/CheckObjectIDTest.java
+++ connectors/cmis/connector/src/test/java/org/apache/manifoldcf/crawler/connectors/cmis/tests/CheckObjectIDTest.java
@@ -1,10 +1,14 @@
 package org.apache.manifoldcf.crawler.connectors.cmis.tests;
 
 import org.apache.manifoldcf.crawler.connectors.cmis.CmisRepositoryConnectorUtils;
+import org.junit.Ignore;
+import org.junit.Test;
 
 public class CheckObjectIDTest {
 
-  public static void main(String[] args) {
+  @Ignore
+  @Test
+  public void testSimple() {
     String cmisQuery = " select cmis:name from cmis:folder where cmis:name='Colacem'";
     cmisQuery = CmisRepositoryConnectorUtils.getCmisQueryWithObjectId(cmisQuery);
     System.out.println(cmisQuery);
