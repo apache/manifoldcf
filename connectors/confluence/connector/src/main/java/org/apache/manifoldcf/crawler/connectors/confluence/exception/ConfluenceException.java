78b809b4098038d782423207e6e1a8fb64a5b5a8
Karl Wright <kwright@apache.org>
2015-11-29 11:57:34 +0000

    Reformat as per MCF standards
    git-svn-id: https://svn.apache.org/repos/asf/manifoldcf/trunk@1717048 13f79535-47bb-0310-9956-ffa450edef68

--- connectors/confluence/connector/src/main/java/org/apache/manifoldcf/crawler/connectors/confluence/exception/ConfluenceException.java
+++ connectors/confluence/connector/src/main/java/org/apache/manifoldcf/crawler/connectors/confluence/exception/ConfluenceException.java
@@ -19,16 +19,16 @@ package org.apache.manifoldcf.crawler.connectors.confluence.exception;
 
 public class ConfluenceException extends Exception {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 5903550079897330304L;
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 5903550079897330304L;
 
-	public ConfluenceException(String message) {
-		super(message);
-	}
-	
-	public ConfluenceException(String message, Throwable cause) {
-		super(message, cause);
-	}
+  public ConfluenceException(String message) {
+    super(message);
+  }
+  
+  public ConfluenceException(String message, Throwable cause) {
+    super(message, cause);
+  }
 }
