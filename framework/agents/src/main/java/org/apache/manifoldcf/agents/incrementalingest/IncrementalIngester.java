7a410ee46adac173bfccab102892e8bbff3df191
Karl Wright <kwright@apache.org>
2016-06-13 07:17:31 +0000

    Fix for CONNECTORS-1323.
    git-svn-id: https://svn.apache.org/repos/asf/manifoldcf/trunk@1748101 13f79535-47bb-0310-9956-ffa450edef68

--- framework/agents/src/main/java/org/apache/manifoldcf/agents/incrementalingest/IncrementalIngester.java
+++ framework/agents/src/main/java/org/apache/manifoldcf/agents/incrementalingest/IncrementalIngester.java
@@ -982,7 +982,7 @@ public class IncrementalIngester extends org.apache.manifoldcf.core.database.Bas
   public void documentDeleteMultiple(
     IPipelineConnections pipelineConnections,
     String[] identifierClasses, String[] identifierHashes,
-    IOutputRemoveActivity activities)
+    IOutputRemoveActivity originalActivities)
     throws ManifoldCFException, ServiceInterruption
   {
     String[] outputConnectionNames = pipelineConnections.getOutputConnectionNames();
@@ -994,7 +994,7 @@ public class IncrementalIngester extends org.apache.manifoldcf.core.database.Bas
       String outputConnectionName = outputConnectionNames[z];
       IOutputConnection connection = outputConnections[z];
 
-      activities = new OutputRemoveActivitiesWrapper(activities,outputConnectionName);
+      IOutputRemoveActivity activities = new OutputRemoveActivitiesWrapper(originalActivities,outputConnectionName);
 
       if (Logging.ingest.isDebugEnabled())
       {
@@ -1039,7 +1039,9 @@ public class IncrementalIngester extends org.apache.manifoldcf.core.database.Bas
         for (DeleteInfo uri : uris)
         {
           if (uri.getURI() != null)
+          {
             removeDocument(connection,uri.getURI(),uri.getOutputVersion(),activities);
+          }
         }
 
         // Now, get rid of all rows that match the given uris.
