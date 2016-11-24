75a0ff7dcdb249313ba756b66b8fafed70980751
Markus Schuch <markus.schuch@deutschebahn.com>
2016-11-24 15:17:11 +0100

    enabled markdown messages

--- connectors/slack/connector/src/main/java/org/apache/manifoldcf/crawler/notifications/slack/SlackMessage.java
+++ connectors/slack/connector/src/main/java/org/apache/manifoldcf/crawler/notifications/slack/SlackMessage.java
@@ -24,6 +24,8 @@ public class SlackMessage {
 
   private String text;
 
+  private boolean mrkdwn = true;
+
   public String getChannel() {
     return channel;
   }
@@ -38,6 +40,14 @@ public class SlackMessage {
 
   public void setText(String text) {
     this.text = text;
+  }
+
+  public boolean isMrkdwn() {
+    return mrkdwn;
+  }
+
+  public void setMrkdwn(boolean mrkdwn) {
+    this.mrkdwn = mrkdwn;
   }
 
 }
