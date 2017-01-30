c1cbdb19c98b1b33a061c2df983cff2795bee6dd
Piergiorgio Lucidi <piergiorgio@apache.org>
2017-01-30 20:17:33 +0000

    First implementation of the CMIS Output Connector: debugging needed (CONNECTORS-1356)
    git-svn-id: https://svn.apache.org/repos/asf/manifoldcf/branches/CONNECTORS-1356@1780980 13f79535-47bb-0310-9956-ffa450edef68

---
+++ connectors/cmis/connector/src/main/java/org/apache/manifoldcf/agents/output/cmisoutput/ColumnSet.java
@@ -0,0 +1,61 @@
+/* $Id$ */
+
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.manifoldcf.agents.output.cmisoutput;
+
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * 
+ * @author Piergiorgio Lucidi
+ *
+ */
+public class ColumnSet {
+
+    private String name = StringUtils.EMPTY;
+    private String alias = StringUtils.EMPTY;
+
+    public ColumnSet() {}
+
+    public ColumnSet(String name, String alias) {
+        this.name = name;
+        this.alias = alias;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    @Override
+    public String toString() {
+        return "ColumnSet{name=" + name + ", alias=" + alias + '}';
+    }
+
+}
