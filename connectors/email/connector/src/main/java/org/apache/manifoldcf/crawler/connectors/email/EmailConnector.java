91885c1197ea78b11a8c4a17ba8f1370fe9dfc4a
Furkan Kamaci <kamaci@apache.org>
2017-04-13 10:59:34 +0000

    Subject metadata for CONNECTORS-1402
    git-svn-id: https://svn.apache.org/repos/asf/manifoldcf/trunk@1791226 13f79535-47bb-0310-9956-ffa450edef68

--- connectors/email/connector/src/main/java/org/apache/manifoldcf/crawler/connectors/email/EmailConnector.java
+++ connectors/email/connector/src/main/java/org/apache/manifoldcf/crawler/connectors/email/EmailConnector.java
@@ -839,7 +839,9 @@ public class EmailConnector extends org.apache.manifoldcf.crawler.connectors.Bas
                     fromStr[j] = address.toString();
                   }
                   rd.addField(EmailConfig.EMAIL_FROM, fromStr);
-
+                } else if (metadata.toLowerCase(Locale.ROOT).equals(EmailConfig.EMAIL_SUBJECT)) {
+                  String subject = msg.getSubject();
+                  rd.addField(EmailConfig.EMAIL_SUBJECT, subject);
                 } else if (metadata.toLowerCase(Locale.ROOT).equals(EmailConfig.EMAIL_DATE)) {
                   rd.addField(EmailConfig.EMAIL_DATE, sentDate.toString());
                 }
