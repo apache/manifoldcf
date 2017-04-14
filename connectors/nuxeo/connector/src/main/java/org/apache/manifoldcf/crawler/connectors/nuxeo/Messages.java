c2c52596fdfaf43f34c9e829608e81526f26f770
Rafa Haro <rharo@apache.org>
2017-04-14 21:26:16 +0000

    CONNECTORS-1290: Upgrading Branch to New Connector version using official Nuxeo Java Client
    git-svn-id: https://svn.apache.org/repos/asf/manifoldcf/branches/CONNECTORS-1290-2@1791420 13f79535-47bb-0310-9956-ffa450edef68

---
+++ connectors/nuxeo/connector/src/main/java/org/apache/manifoldcf/crawler/connectors/nuxeo/Messages.java
@@ -0,0 +1,105 @@
+package org.apache.manifoldcf.crawler.connectors.nuxeo;
+
+import java.util.Locale;
+import java.util.Map;
+
+import org.apache.manifoldcf.core.interfaces.IHTTPOutput;
+import org.apache.manifoldcf.core.interfaces.ManifoldCFException;
+
+public class Messages extends org.apache.manifoldcf.ui.i18n.Messages {
+
+	public static final String DEFAULT_BUNDLE_NAME = "org.apache.manifoldcf.crawler.connectors.nuxeo.common";
+	public static final String DEFAULT_PATH_NAME = "org.apache.manifoldcf.crawler.connectors.nuxeo";
+
+	/** Constructor - do no instantiate
+	 * @
+	  */
+	  protected Messages()
+	  {
+	  }
+
+	public static String getString(Locale locale, String messageKey) {
+		return getString(DEFAULT_BUNDLE_NAME, locale, messageKey, null);
+	}
+
+	public static String getAttributeString(Locale locale, String messageKey) {
+		return getAttributeString(DEFAULT_BUNDLE_NAME, locale, messageKey, null);
+	}
+
+	public static String getBodyString(Locale locale, String messageKey) {
+		return getBodyString(DEFAULT_BUNDLE_NAME, locale, messageKey, null);
+	}
+
+	public static String getAttributeJavascriptString(Locale locale, String messageKey) {
+		return getAttributeJavascriptString(DEFAULT_BUNDLE_NAME, locale, messageKey, null);
+	}
+
+	public static String getBodyJavascriptString(Locale locale, String messageKey) {
+		return getBodyJavascriptString(DEFAULT_BUNDLE_NAME, locale, messageKey, null);
+	}
+
+	public static String getString(Locale locale, String messageKey, Object[] args) {
+		return getString(DEFAULT_BUNDLE_NAME, locale, messageKey, args);
+	}
+
+	public static String getAttributeString(Locale locale, String messageKey, Object[] args) {
+		return getAttributeString(DEFAULT_BUNDLE_NAME, locale, messageKey, args);
+	}
+
+	public static String getBodyString(Locale locale, String messageKey, Object[] args) {
+		return getBodyString(DEFAULT_BUNDLE_NAME, locale, messageKey, args);
+	}
+
+	public static String getAttributeJavascriptString(Locale locale, String messageKey, Object[] args) {
+		return getAttributeJavascriptString(DEFAULT_BUNDLE_NAME, locale, messageKey, args);
+	}
+
+	public static String getBodyJavascriptString(Locale locale, String messageKey, Object[] args) {
+		return getBodyJavascriptString(DEFAULT_BUNDLE_NAME, locale, messageKey, args);
+	}
+
+	// More general methods which allow bundlenames and class loaders to be
+	// specified.
+
+	public static String getString(String bundleName, Locale locale, String messageKey, Object[] args) {
+		return getString(Messages.class, bundleName, locale, messageKey, args);
+	}
+
+	public static String getAttributeString(String bundleName, Locale locale, String messageKey, Object[] args) {
+		return getAttributeString(Messages.class, bundleName, locale, messageKey, args);
+	}
+
+	public static String getBodyString(String bundleName, Locale locale, String messageKey, Object[] args) {
+		return getBodyString(Messages.class, bundleName, locale, messageKey, args);
+	}
+
+	public static String getAttributeJavascriptString(String bundleName, Locale locale, String messageKey,
+			Object[] args) {
+		return getAttributeJavascriptString(Messages.class, bundleName, locale, messageKey, args);
+	}
+
+	public static String getBodyJavascriptString(String bundleName, Locale locale, String messageKey, Object[] args) {
+		return getBodyJavascriptString(Messages.class, bundleName, locale, messageKey, args);
+	}
+
+	// Resource output
+
+	public static void outputResource(IHTTPOutput output, Locale locale, String resourceKey,
+			Map<String, String> substitutionParameters, boolean mapToUpperCase) throws ManifoldCFException {
+		outputResource(output, Messages.class, DEFAULT_PATH_NAME, locale, resourceKey, substitutionParameters,
+				mapToUpperCase);
+	}
+
+	public static void outputResourceWithVelocity(IHTTPOutput output, Locale locale, String resourceKey,
+			Map<String, String> substitutionParameters, boolean mapToUpperCase) throws ManifoldCFException {
+		outputResourceWithVelocity(output, Messages.class, DEFAULT_BUNDLE_NAME, DEFAULT_PATH_NAME, locale, resourceKey,
+				substitutionParameters, mapToUpperCase);
+	}
+
+	public static void outputResourceWithVelocity(IHTTPOutput output, Locale locale, String resourceKey,
+			Map<String, Object> contextObjects) throws ManifoldCFException {
+		outputResourceWithVelocity(output, Messages.class, DEFAULT_BUNDLE_NAME, DEFAULT_PATH_NAME, locale, resourceKey,
+				contextObjects);
+	}
+
+}
