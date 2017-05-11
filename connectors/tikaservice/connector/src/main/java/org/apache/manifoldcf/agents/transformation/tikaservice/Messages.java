1b09ab9940390f0b2087f2b2ca03b9318ad1be7b
Karl Wright <kwright@apache.org>
2017-05-11 11:23:35 +0000

    Move everything to the tikaservice package
    git-svn-id: https://svn.apache.org/repos/asf/manifoldcf/branches/CONNECTORS-1425@1794808 13f79535-47bb-0310-9956-ffa450edef68

--- connectors/tikaservice/connector/src/main/java/org/apache/manifoldcf/agents/transformation/tikaservice/Messages.java
+++ connectors/tikaservice/connector/src/main/java/org/apache/manifoldcf/agents/transformation/tikaservice/Messages.java
@@ -16,7 +16,7 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-package org.apache.manifoldcf.agents.transformation.tika;
+package org.apache.manifoldcf.agents.transformation.tikaservice;
 
 import java.util.Locale;
 import java.util.Map;
