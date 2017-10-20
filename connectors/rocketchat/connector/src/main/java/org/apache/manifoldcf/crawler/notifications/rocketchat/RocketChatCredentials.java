38a864c1943bee789ad72b3978cbb09fbc0f181e
Markus Schuch <schuch@apache.org>
2017-10-20 22:23:24 +0000

    Implemented the Rocket.Chat notification connector
    git-svn-id: https://svn.apache.org/repos/asf/manifoldcf/branches/CONNECTORS-1468@1812793 13f79535-47bb-0310-9956-ffa450edef68

---
+++ connectors/rocketchat/connector/src/main/java/org/apache/manifoldcf/crawler/notifications/rocketchat/RocketChatCredentials.java
@@ -0,0 +1,25 @@
+package org.apache.manifoldcf.crawler.notifications.rocketchat;
+
+public class RocketChatCredentials {
+
+  private String user;
+  
+  private String password;
+
+  public String getUser() {
+    return user;
+  }
+
+  public void setUser(String user) {
+    this.user = user;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+  
+}
