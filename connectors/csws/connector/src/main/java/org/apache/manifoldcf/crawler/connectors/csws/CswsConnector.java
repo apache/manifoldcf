2aaf73b68fb17b1e2ac70aba3e52fa59dce479ae
Markus Schuch <schuch@apache.org>
2019-08-26 21:57:53 +0000

    Fix javadoc errors
    git-svn-id: https://svn.apache.org/repos/asf/manifoldcf/branches/CONNECTORS-1566@1865957 13f79535-47bb-0310-9956-ffa450edef68

--- connectors/csws/connector/src/main/java/org/apache/manifoldcf/crawler/connectors/csws/CswsConnector.java
+++ connectors/csws/connector/src/main/java/org/apache/manifoldcf/crawler/connectors/csws/CswsConnector.java
@@ -854,7 +854,7 @@ public class CswsConnector extends org.apache.manifoldcf.crawler.connectors.Base
   *@param activities is the interface this method should use to perform whatever framework actions are desired.
   *@param spec is a document specification (that comes from the job).
   *@param seedTime is the end of the time range of documents to consider, exclusive.
-  *@param lastSeedVersionString is the last seeding version string for this job, or null if the job has no previous seeding version string.
+  *@param lastSeedVersion is the last seeding version string for this job, or null if the job has no previous seeding version string.
   *@param jobMode is an integer describing how the job is being run, whether continuous or once-only.
   *@return an updated seeding version string, to be stored with the job.
   */
@@ -1407,7 +1407,7 @@ public class CswsConnector extends org.apache.manifoldcf.crawler.connectors.Base
   
   /** Output the configuration body section.
   * This method is called in the body section of the connector's configuration page.  Its purpose is to present the required form elements for editing.
-  * The coder can presume that the HTML that is output from this configuration will be within appropriate <html>, <body>, and <form> tags.  The name of the
+  * The coder can presume that the HTML that is output from this configuration will be within appropriate &lt;html&gt;, &lt;body&gt;, and &lt;form&gt; tags.  The name of the
   * form is "editconnection".
   *@param threadContext is the local thread context.
   *@param out is the output to which any HTML should be sent.
@@ -1696,7 +1696,7 @@ public class CswsConnector extends org.apache.manifoldcf.crawler.connectors.Base
   
   /** View configuration.
   * This method is called in the body section of the connector's view configuration page.  Its purpose is to present the connection information to the user.
-  * The coder can presume that the HTML that is output from this configuration will be within appropriate <html> and <body> tags.
+  * The coder can presume that the HTML that is output from this configuration will be within appropriate &lt;html&gt; and &lt;body&gt; tags.
   *@param threadContext is the local thread context.
   *@param out is the output to which any HTML should be sent.
   *@param parameters are the configuration parameters, as they currently exist, for this connection being configured.
@@ -1767,7 +1767,7 @@ public class CswsConnector extends org.apache.manifoldcf.crawler.connectors.Base
   * This method is called in the body section of a job page which has selected a repository connection of the
   * current type.  Its purpose is to present the required form elements for editing.
   * The coder can presume that the HTML that is output from this configuration will be within appropriate
-  *  <html>, <body>, and <form> tags.  The name of the form is always "editjob".
+  *  &lt;html&gt;, &lt;body&gt;, and &lt;form&gt; tags.  The name of the form is always "editjob".
   * The connector will be connected before this method can be called.
   *@param out is the output to which any HTML should be sent.
   *@param locale is the locale the output is preferred to be in.
@@ -2580,7 +2580,7 @@ public class CswsConnector extends org.apache.manifoldcf.crawler.connectors.Base
   /** View specification.
   * This method is called in the body section of a job's view page.  Its purpose is to present the document
   * specification information to the user.  The coder can presume that the HTML that is output from
-  * this configuration will be within appropriate <html> and <body> tags.
+  * this configuration will be within appropriate &lt;html&gt; and &lt;body&gt; tags.
   * The connector will be connected before this method can be called.
   *@param out is the output to which any HTML should be sent.
   *@param locale is the locale the output is preferred to be in.
@@ -3458,7 +3458,6 @@ public class CswsConnector extends org.apache.manifoldcf.crawler.connectors.Base
 
   /** Get an object's rights.  This will be an array of right id's, including the special
   * ones defined by Csws, or null will be returned (if the object is not found).
-  *@param vol is the volume id
   *@param objID is the object id
   *@return the NodeRights object
   */
