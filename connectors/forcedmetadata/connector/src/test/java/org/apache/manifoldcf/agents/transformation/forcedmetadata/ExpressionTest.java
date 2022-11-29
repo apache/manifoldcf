52c96b1b84604a543f3f5a6246d787ff0639afe0
Markus Schuch <schuch@apache.org>
2022-11-29 07:13:20 +0000

    fix wrong package name
    git-svn-id: https://svn.apache.org/repos/asf/manifoldcf/trunk@1905592 13f79535-47bb-0310-9956-ffa450edef68

--- connectors/forcedmetadata/connector/src/test/java/org/apache/manifoldcf/agents/transformation/forcedmetadata/ExpressionTest.java
+++ connectors/forcedmetadata/connector/src/test/java/org/apache/manifoldcf/agents/transformation/forcedmetadata/ExpressionTest.java
@@ -16,7 +16,7 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-package org.apache.manifoldcf.crawler.connectors.webcrawler;
+package org.apache.manifoldcf.agents.transformation.forcedmetadata;
 
 import org.apache.manifoldcf.agents.transformation.forcedmetadata.*;
 import org.apache.manifoldcf.agents.interfaces.RepositoryDocument;
