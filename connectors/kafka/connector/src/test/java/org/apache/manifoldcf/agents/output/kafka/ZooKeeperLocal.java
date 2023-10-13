671eab800f4956b35b583d927499ff4e3a5a595c
Karl Wright <kwright@apache.org>
2023-10-13 12:53:05 +0000

    Fix kafka test build error
    git-svn-id: https://svn.apache.org/repos/asf/manifoldcf/trunk@1912938 13f79535-47bb-0310-9956-ffa450edef68

--- connectors/kafka/connector/src/test/java/org/apache/manifoldcf/agents/output/kafka/ZooKeeperLocal.java
+++ connectors/kafka/connector/src/test/java/org/apache/manifoldcf/agents/output/kafka/ZooKeeperLocal.java
@@ -20,6 +20,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 import org.apache.zookeeper.server.ServerConfig;
+import org.apache.zookeeper.server.admin.AdminServer.AdminServerException;
 import org.apache.zookeeper.server.ZooKeeperServerMain;
 import org.apache.zookeeper.server.quorum.QuorumPeerConfig;
 
@@ -43,10 +44,14 @@ public class ZooKeeperLocal {
       public void run() {
         try {
           zooKeeperServer.runFromConfig(configuration);
+        } catch (AdminServerException e) {
+          System.out.println("ZooKeeper Failed");
+          e.printStackTrace(System.err);
         } catch (IOException e) {
           System.out.println("ZooKeeper Failed");
           e.printStackTrace(System.err);
         }
+        
       }
     }.start();
   }
