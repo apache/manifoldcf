31a14dbd971091cd18e1fdb7c419bb6aff39d556
Piergiorgio Lucidi <plucidi@ziaconsulting.com>
2025-07-25 11:33:42 +0200

    Removed MD5 generation script

--- sign-release-artifacts.sh
+++ sign-release-artifacts.sh
@@ -24,13 +24,6 @@ gpg --local-user "$1" --armor --output apache-manifoldcf-$2-lib.tar.gz.asc --det
 gpg --local-user "$1" --armor --output apache-manifoldcf-$2-bin.zip.asc --detach-sig apache-manifoldcf-$2-bin.zip
 gpg --local-user "$1" --armor --output apache-manifoldcf-$2-bin.tar.gz.asc --detach-sig apache-manifoldcf-$2-bin.tar.gz
 
-gpg --print-md MD5 apache-manifoldcf-$2-src.zip > apache-manifoldcf-$2-src.zip.md5
-gpg --print-md MD5 apache-manifoldcf-$2-src.tar.gz > apache-manifoldcf-$2-src.tar.gz.md5
-gpg --print-md MD5 apache-manifoldcf-$2-lib.zip > apache-manifoldcf-$2-lib.zip.md5
-gpg --print-md MD5 apache-manifoldcf-$2-lib.tar.gz > apache-manifoldcf-$2-lib.tar.gz.md5
-gpg --print-md MD5 apache-manifoldcf-$2-bin.zip > apache-manifoldcf-$2-bin.zip.md5
-gpg --print-md MD5 apache-manifoldcf-$2-bin.tar.gz > apache-manifoldcf-$2-bin.tar.gz.md5
-
 gpg --print-md SHA512 apache-manifoldcf-$2-src.zip > apache-manifoldcf-$2-src.zip.sha512
 gpg --print-md SHA512 apache-manifoldcf-$2-src.tar.gz > apache-manifoldcf-$2-src.tar.gz.sha512
 gpg --print-md SHA512 apache-manifoldcf-$2-lib.zip > apache-manifoldcf-$2-lib.zip.sha512
