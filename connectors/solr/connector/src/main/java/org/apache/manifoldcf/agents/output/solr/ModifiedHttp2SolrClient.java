3adbfc2b855e77c26fb7636cac20407b9e102e60
Piergiorgio Lucidi <plucidi@ziaconsulting.com>
2026-03-17 14:51:58 +0100

    Maven deps are now aligned with Ant (CONNECTORS-1779), updated GitHub Workflows and RAT

--- connectors/solr/connector/src/main/java/org/apache/manifoldcf/agents/output/solr/ModifiedHttp2SolrClient.java
+++ connectors/solr/connector/src/main/java/org/apache/manifoldcf/agents/output/solr/ModifiedHttp2SolrClient.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.manifoldcf.agents.output.solr;
 
 import static org.apache.solr.common.util.Utils.getObjectByPath;
@@ -662,7 +678,7 @@ public class ModifiedHttp2SolrClient extends SolrClient {
         final String[] vals = wparams.getParams(key);
         if (vals != null) {
           for (final String val : vals) {
-            content.addPart(new org.eclipse.jetty.http.MultiPart.ContentSourcePart(key, null, null, new StringRequestContent(val)));
+            content.addPart(new org.eclipse.jetty.http.MultiPart.ContentSourcePart(key, null, HttpFields.EMPTY, new StringRequestContent(val)));
           }
         }
       }
@@ -678,7 +694,7 @@ public class ModifiedHttp2SolrClient extends SolrClient {
           }
           final org.eclipse.jetty.http.HttpFields.Mutable fields = org.eclipse.jetty.http.HttpFields.build();
           fields.add(HttpHeader.CONTENT_TYPE, contentType);
-          content.addPart(new org.eclipse.jetty.http.MultiPart.ContentSourcePart(name, contentStream.getName(), fields, new InputStreamRequestContent(contentStream.getStream())));
+          content.addPart(new org.eclipse.jetty.http.MultiPart.ContentSourcePart(name, contentStream.getName(), fields.asImmutable(), new InputStreamRequestContent(contentStream.getStream())));
         }
       }
       req.body(content);
